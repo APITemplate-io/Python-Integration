4a212a2b0e2ab56417668f35c85c34d8c79396ca
dev <dev@ubuntu>
2020-12-09 21:51:05 +0800

    first commit

---
+++ test.py
@@ -0,0 +1,40 @@
+from apitemplateio_lib import APITemplateIO
+
+api_key = "<Your API Key>"
+
+def test_pdf():
+    template_id = "<your pdf template id>"
+    data = {
+        "name": "hello world"
+    }
+    save_to = "/home/dev/result.pdf"
+
+    apitemplate = APITemplateIO(api_key)
+    apitemplate.create_pdf(template_id, data, save_to)
+
+def test_image():
+    template_id = "<your image template id>"
+    data = {
+        "overrides": [
+            {
+                "name" : "text_1",
+                "text" : "hello world",
+            }
+        ]
+    }
+    save_to_jpeg = "/home/dev/result.jpeg"
+    save_to_png = "/home/dev/result.png"
+
+    apitemplate = APITemplateIO(api_key)
+    apitemplate.create_image(template_id, data, save_to_jpeg, save_to_png)
+
+def test_account_information():
+    apitemplate = APITemplateIO(api_key)
+    print(apitemplate.get_account_information())
+
+
+if __name__ == "__main__":
+    test_pdf()
+    test_image()
+    test_account_information()
+
